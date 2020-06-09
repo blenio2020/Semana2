8632b0d0482e00e9addaa52022f1d7d282c22459
Julio Decima <juliodecima@gmail.com>
2020-06-09 08:04:47 -0300

    commit inicial

---
+++ js/funciones.js
@@ -0,0 +1,67 @@
+var bd;
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function fetchJSONFile(path, callback) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function() {
+      if (httpRequest.readyState === 4) {
+          if (httpRequest.status === 200) {
+              var data = JSON.parse(httpRequest.responseText);
+              if (callback) callback(data);
+          }
+      }
+  };
+  httpRequest.open('GET', path);
+  httpRequest.send(); 
+}
+
+// this requests the file and executes a callback with the parsed result once
+//   it is available
+fetchJSONFile('../datos/db.json', function(data){
+  // do something with your data
+  bd=data.proyectos
+  bd.map(datos=>document.getElementById("trabajos").insertRow(-1).innerHTML = `<td>${datos.nombre}</td><td>${datos.lenguaje}</td><td>${datos.monto} dolares</td>`)
+});
+
+function insertar(e){
+  e.preventDefault()
+  objetoTemporal={nombre:document.getElementById('nombre').value,lenguaje:document.getElementById('lenguaje').value,monto:document.getElementById('monto').value}
+  bd.push(objetoTemporal)
+  document.getElementById("trabajos").insertRow(-1).innerHTML = `<td>${objetoTemporal.nombre}</td><td>${objetoTemporal.lenguaje}</td><td>${objetoTemporal.monto} dolares</td>`
+  document.getElementById("nombre").value=""
+  document.getElementById("lenguaje").value=""
+  document.getElementById("monto").value=""
+
+}
+
+var contenido = document.querySelector('#mensajes')
+        function traer() {
+            fetch('https://randomuser.me/api/')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.results['0'])
+                datos=data.results['0']
+                let objetoTemporal={nombre:datos.name,telefono:datos.phone,mensaje:"Contactame por favor"}
+                document.getElementById("listaMensajes").insertRow(-1).innerHTML = `<td>${objetoTemporal.nombre.last}</td><td>${objetoTemporal.telefono}</td><td>${objetoTemporal.mensaje} dolares</td>`      
+            })
+        } 
